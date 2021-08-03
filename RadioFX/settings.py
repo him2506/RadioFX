688112603b1158894b9179fb440b1e7b96dad358
him2506 <iitianhimanchalsing@gmail.com>
2021-08-03 15:43:18 +0530

    cc

--- RadioFX/settings.py
+++ RadioFX/settings.py
@@ -48,6 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'RadioFX.urls'
@@ -118,14 +119,26 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+# STATIC_URL = '/static/'
+# # static file ko add kiya inside static folder of BASE_DIR.
+
+# # STATICFILES_DIRS = [
+# #     os.path.join(BASE_DIR, "static"),
+# # ]
+# STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
+# # Adding media directory
+# MEDIA_ROOT= os.path.join(BASE_DIR,'media')
+# MEDIA_URL = '/media/'
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-# static file ko add kiya inside static folder of BASE_DIR.
 
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, "static"),
-# ]
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-
-# Adding media directory
-MEDIA_ROOT= os.path.join(BASE_DIR,'media')
-MEDIA_URL = '/media/'
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
